343a9944a15994092385735258fe4cb716420b62
josh-lindsay <122793231+josh-lindsay@users.noreply.github.com>
2023-02-01 15:12:06 +0000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Email_Sender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSendVerifyCode = new System.Windows.Forms.Button();
+            this.txtUserEmail = new System.Windows.Forms.TextBox();
+            this.pnlEnterEmail = new System.Windows.Forms.Panel();
+            this.pnlEnterCode = new System.Windows.Forms.Panel();
+            this.btnSendAnotherCode = new System.Windows.Forms.Button();
+            this.btnCheckCode = new System.Windows.Forms.Button();
+            this.txtEnteredCode = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlCreateNewPassword = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNewPassword = new System.Windows.Forms.TextBox();
+            this.btnAppendPassword = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnlEnterEmail.SuspendLayout();
+            this.pnlEnterCode.SuspendLayout();
+            this.pnlCreateNewPassword.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSendVerifyCode
+            // 
+            this.btnSendVerifyCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSendVerifyCode.Location = new System.Drawing.Point(47, 29);
+            this.btnSendVerifyCode.Name = "btnSendVerifyCode";
+            this.btnSendVerifyCode.Size = new System.Drawing.Size(134, 49);
+            this.btnSendVerifyCode.TabIndex = 0;
+            this.btnSendVerifyCode.Text = "Forgot Password?";
+            this.btnSendVerifyCode.UseVisualStyleBackColor = true;
+            this.btnSendVerifyCode.Click += new System.EventHandler(this.btnSendVerifyCode_Click);
+            // 
+            // txtUserEmail
+            // 
+            this.txtUserEmail.Location = new System.Drawing.Point(3, 3);
+            this.txtUserEmail.Name = "txtUserEmail";
+            this.txtUserEmail.Size = new System.Drawing.Size(220, 20);
+            this.txtUserEmail.TabIndex = 1;
+            // 
+            // pnlEnterEmail
+            // 
+            this.pnlEnterEmail.Controls.Add(this.txtUserEmail);
+            this.pnlEnterEmail.Controls.Add(this.btnSendVerifyCode);
+            this.pnlEnterEmail.Location = new System.Drawing.Point(2, 114);
+            this.pnlEnterEmail.Name = "pnlEnterEmail";
+            this.pnlEnterEmail.Size = new System.Drawing.Size(229, 85);
+            this.pnlEnterEmail.TabIndex = 3;
+            // 
+            // pnlEnterCode
+            // 
+            this.pnlEnterCode.Controls.Add(this.btnSendAnotherCode);
+            this.pnlEnterCode.Controls.Add(this.btnCheckCode);
+            this.pnlEnterCode.Controls.Add(this.txtEnteredCode);
+            this.pnlEnterCode.Controls.Add(this.label1);
+            this.pnlEnterCode.Location = new System.Drawing.Point(266, 114);
+            this.pnlEnterCode.Name = "pnlEnterCode";
+            this.pnlEnterCode.Size = new System.Drawing.Size(190, 95);
+            this.pnlEnterCode.TabIndex = 4;
+            // 
+            // btnSendAnotherCode
+            // 
+            this.btnSendAnotherCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSendAnotherCode.Location = new System.Drawing.Point(95, 44);
+            this.btnSendAnotherCode.Name = "btnSendAnotherCode";
+            this.btnSendAnotherCode.Size = new System.Drawing.Size(92, 48);
+            this.btnSendAnotherCode.TabIndex = 3;
+            this.btnSendAnotherCode.Text = "Send Another Code";
+            this.btnSendAnotherCode.UseVisualStyleBackColor = true;
+            this.btnSendAnotherCode.Click += new System.EventHandler(this.btnSendAnotherCode_Click);
+            // 
+            // btnCheckCode
+            // 
+            this.btnCheckCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheckCode.Location = new System.Drawing.Point(3, 44);
+            this.btnCheckCode.Name = "btnCheckCode";
+            this.btnCheckCode.Size = new System.Drawing.Size(86, 48);
+            this.btnCheckCode.TabIndex = 2;
+            this.btnCheckCode.Text = "Reset Password";
+            this.btnCheckCode.UseVisualStyleBackColor = true;
+            this.btnCheckCode.Click += new System.EventHandler(this.btnCheckCode_Click);
+            // 
+            // txtEnteredCode
+            // 
+            this.txtEnteredCode.Location = new System.Drawing.Point(70, 18);
+            this.txtEnteredCode.Name = "txtEnteredCode";
+            this.txtEnteredCode.Size = new System.Drawing.Size(96, 20);
+            this.txtEnteredCode.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please enter your verification code here:";
+            // 
+            // pnlCreateNewPassword
+            // 
+            this.pnlCreateNewPassword.Controls.Add(this.btnAppendPassword);
+            this.pnlCreateNewPassword.Controls.Add(this.pictureBox1);
+            this.pnlCreateNewPassword.Controls.Add(this.txtNewPassword);
+            this.pnlCreateNewPassword.Controls.Add(this.label2);
+            this.pnlCreateNewPassword.Location = new System.Drawing.Point(5, 8);
+            this.pnlCreateNewPassword.Name = "pnlCreateNewPassword";
+            this.pnlCreateNewPassword.Size = new System.Drawing.Size(269, 100);
+            this.pnlCreateNewPassword.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(-4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(216, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Enter a new password below:";
+            // 
+            // txtNewPassword
+            // 
+            this.txtNewPassword.Location = new System.Drawing.Point(4, 24);
+            this.txtNewPassword.Name = "txtNewPassword";
+            this.txtNewPassword.PasswordChar = '*';
+            this.txtNewPassword.Size = new System.Drawing.Size(216, 20);
+            this.txtNewPassword.TabIndex = 1;
+            // 
+            // btnAppendPassword
+            // 
+            this.btnAppendPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAppendPassword.Location = new System.Drawing.Point(4, 50);
+            this.btnAppendPassword.Name = "btnAppendPassword";
+            this.btnAppendPassword.Size = new System.Drawing.Size(216, 46);
+            this.btnAppendPassword.TabIndex = 3;
+            this.btnAppendPassword.Text = "Set as new password";
+            this.btnAppendPassword.UseVisualStyleBackColor = true;
+            this.btnAppendPassword.Click += new System.EventHandler(this.btnAppendPassword_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Email_Sender.Properties.Resources.eye_closed;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(226, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(39, 30);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 239);
+            this.Controls.Add(this.pnlCreateNewPassword);
+            this.Controls.Add(this.pnlEnterCode);
+            this.Controls.Add(this.pnlEnterEmail);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlEnterEmail.ResumeLayout(false);
+            this.pnlEnterEmail.PerformLayout();
+            this.pnlEnterCode.ResumeLayout(false);
+            this.pnlEnterCode.PerformLayout();
+            this.pnlCreateNewPassword.ResumeLayout(false);
+            this.pnlCreateNewPassword.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSendVerifyCode;
+        private System.Windows.Forms.TextBox txtUserEmail;
+        private System.Windows.Forms.Panel pnlEnterEmail;
+        private System.Windows.Forms.Panel pnlEnterCode;
+        private System.Windows.Forms.Button btnCheckCode;
+        private System.Windows.Forms.TextBox txtEnteredCode;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSendAnotherCode;
+        private System.Windows.Forms.Panel pnlCreateNewPassword;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtNewPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAppendPassword;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+using System.Reflection.Emit;
+
+namespace Email_Sender
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int recoveryCode;
+        private void recoveryCodeGenerator()
+        {
+            Random rand = new Random();
+            recoveryCode = rand.Next(100000, 999999);
+            MessageBox.Show(recoveryCode.ToString());
+            //recoveryCodeChecker(recoveryCode.ToString());
+        }
+
+        private void recoveryCodeChecker(string recoveryCode)
+        {
+            if (txtEnteredCode.Text == recoveryCode)
+            {
+                MessageBox.Show("Success!");
+                pnlCreateNewPassword.Visible = true;
+                pnlCreateNewPassword.Location = new Point(5, 8);
+                this.Size = new Size(292, 148);
+                pnlEnterCode.Visible = false;
+                pnlEnterEmail.Visible = false;
+
+            }
+            else
+            {
+                MessageBox.Show("Unsuccessfull, please try again");
+            } 
+        }
+
+        private void recoveryCodeEntryPage()
+        {
+            this.Size = new Size(215, 139);
+            pnlEnterEmail.Visible = false;
+            pnlEnterCode.Visible = true;
+            pnlEnterCode.Location = new Point(2, 2);
+        }
+
+        private void newPasswordEntry()
+        {
+
+        }
+
+        private void btnSendVerifyCode_Click(object sender, EventArgs e)
+        {
+            recoveryCodeGenerator();
+            SendEmail();
+            recoveryCodeEntryPage();
+        }
+
+        private void SendEmail()
+        {
+            try
+            {
+                string from = "no.reply.pass.recovery.test@gmail.com";
+                string password = "bejf uvos dnjo ffka";
+                MailMessage msg = new MailMessage();
+                msg.Subject = "Password Reset";
+                msg.From = new MailAddress(from);
+                msg.Body = "Your verification code is: "+ recoveryCode.ToString();
+                msg.To.Add(new MailAddress(txtUserEmail.Text));
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = false;
+                //Encrypts the email to ensure enhanced security.
+                smtp.EnableSsl = true;
+                NetworkCredential nc= new NetworkCredential (from, password);
+                smtp.Credentials = nc;
+                smtp.Send(msg);
+                MessageBox.Show("Email Sent!");
+          
+            }
+            catch 
+            {
+                MessageBox.Show("Email Failed To Send!");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(268, 143);
+            pnlEnterEmail.Visible = true;
+            pnlEnterEmail.Location = new Point(12, 12);
+            pnlEnterCode.Visible = false;
+            pnlCreateNewPassword.Visible = false;
+        }
+
+        private void btnCheckCode_Click(object sender, EventArgs e)
+        {
+            recoveryCodeChecker(recoveryCode.ToString());
+        }
+
+        private void btnSendAnotherCode_Click(object sender, EventArgs e)
+        {
+            recoveryCodeGenerator();
+            SendEmail();
+            //recoveryCodeEntryPage();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            pictureBox1.BackgroundImage = Email_Sender.Properties.Resources.eye_open;
+            txtNewPassword.PasswordChar = '\0';
+            txtNewPassword.UseSystemPasswordChar = false;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            pictureBox1.BackgroundImage = Email_Sender.Properties.Resources.eye_closed;
+            txtNewPassword.PasswordChar = '*';
+            txtNewPassword.UseSystemPasswordChar = false;
+        }
+
+        private void btnAppendPassword_Click(object sender, EventArgs e)
+        {
+            if (txtNewPassword.TextLength >= 8)
+            {
+                MessageBox.Show("Your Password has successully been reset!");
+            }
+            else if (txtNewPassword.TextLength < 8)
+            {
+                MessageBox.Show("Password must be longer than 8 characters!");
+            }
+            
+        }
+    }
+}
